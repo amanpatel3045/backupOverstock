f4dc2aa1a707d4fb6a617a97a462eab11e871d6b
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-13 22:43:16 +0530

    Add files via upload

--- Signup.jsx
+++ Signup.jsx
@@ -1,5 +1,7 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
+
 import {
   Input,
   HStack,
@@ -9,8 +11,52 @@ import {
 } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Divider, Stack } from "@chakra-ui/react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-const Signup = () => {
+function Signup() {
+  const initialvalues = { email: "", password: "" };
+  const [inputValues, setInputValues] = useState(initialvalues);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (inp) => {
+    const { name, value } = inp.target;
+    setInputValues({ ...inputValues, [name]: value });
+    console.log(inputValues);
+  };
+  const handleSignup = (body) => {
+    setLoading(true);
+    fetch(`http://localhost:3000/User`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
+        alert("Signup successful");
+      })
+      .catch(() => alert("Duplicate username"))
+      .finally(() => setLoading(false));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSignup(inputValues);
+  };
+
+  if (loading) {
+    return (
+      <Container mt={100}>
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </Container>
+    );
+  }
+
   return (
     <>
       <Box width="40%" ml="10%">
@@ -21,7 +67,17 @@ const Signup = () => {
           <Text mt="12%" ml="-39%">
             Email Address*
           </Text>
-          <Input width="322px" mt="3%" placeholder="Email" />
+          <Input
+            type="text"
+            autoComplete="off"
+            name="email"
+            id="email"
+            width="322px"
+            mt="3%"
+            placeholder="Email"
+            onChange={handleChange}
+            value={inputValues.email}
+          />
         </Box>
 
         <Box ml="20%">
@@ -30,17 +86,34 @@ const Signup = () => {
             <Text>Confirm Password*</Text>
           </HStack>
           <HStack mt="3%">
-            <Input width="160px" placeholder="Paasword" />
-            <Input width="160px" placeholder="Confirm Password" />
+            <Input
+              type="password"
+              autoComplete="off"
+              name="password"
+              id="password"
+              width="160px"
+              placeholder="Paasword"
+              onChange={handleChange}
+              value={inputValues.password}
+            />
+            <Input
+              type="password"
+              autoComplete="off"
+              name="password2"
+              id="password2"
+              width="160px"
+              placeholder="Confirm Password"
+            />
           </HStack>
         </Box>
-        <Button width="322px" mt="3%" colorScheme="blue">
+        <Button onClick={handleSubmit} width="322px" mt="3%" colorScheme="blue">
           Create Account
         </Button>
         <hr width="23px"></hr>
         <Button width="322px" mt="5%" colorScheme="gray">
           Continue as Guest
         </Button>
+
         <Text mt="4%">
           By creating an account or continuing as a Guest, you
         </Text>
@@ -54,6 +127,7 @@ const Signup = () => {
       </Stack>
     </>
   );
-};
+}
+
 // bg="tomato"
 export default Signup;
